0f5ddf0298e1a70c4feace9f379d8c0cf30795ee
Snopoff <snopoffp@gmail.com>
2026-02-17 16:57:45 -0600

    responses -> completion

--- main.py
+++ main.py
@@ -6,8 +6,16 @@ from omegaconf import DictConfig
 @hydra.main(version_base=None, config_path="configs", config_name="default")
 def main(cfg: DictConfig) -> None:
     client = instantiate(cfg.provider.client)
-    resp = client.responses.create(model=cfg.model.name, input=cfg.prompt)
-    print(resp.output_text)
+    # return responses ...
+    # resp = client.responses.create(model=cfg.model.name, input=cfg.prompt)
+    # print(resp.output_text)
+
+    resp = client.chat.completions.create(
+        model=cfg.model.name,
+        messages=[{"role": cfg.model.role, "content": cfg.prompt}],
+    )
+
+    print(resp.choices[0].message.content)
 
 
 if __name__ == "__main__":
